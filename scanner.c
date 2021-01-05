cb829c3bc325a5bd25fd4f2ede59b4c4ca6bf66e
Liviu Strati <liviu.strati@hdt.ro>
2021-01-05 15:36:35 +0200

    error correction

--- scanner.c
+++ scanner.c
@@ -134,10 +134,10 @@ void get_radio_parameters(const u_char *packet, int len)
 
 void get_frame_parameters(const u_char *packet, int len)
 {
-    struct radiotap_header *radiotap = (struct radiotap_header *)packet;
-    struct mgmt_header_t *mgmt_frame = (struct mgmt_header_t *)(packet + radiotap.it_len);
+    struct ieee80211_radiotap_header *radiotap = (struct ieee80211_radiotap_header *)packet;
+    struct mgmt_header_t *mgmt_frame = (struct mgmt_header_t *)(packet + radiotap->it_len);
 
-    printf("SA MAC:%u\n",mgmt_frame.sa);
+    printf("SA MAC:%u\n",mgmt_frame->sa);
 
 }
 
