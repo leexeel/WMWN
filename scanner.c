c0be2186f920fcf2191c2fef86943942a57b23ed
Liviu Strati <liviu.strati@hdt.ro>
2021-01-05 17:00:14 +0200

    prinf frame controll bytes

--- scanner.c
+++ scanner.c
@@ -146,19 +146,20 @@ void get_frame_parameters(const u_char *packet, const struct pcap_pkthdr *header
 {
     if (header->caplen < sizeof(struct ieee80211_radiotap_header))
     {
-        printf("Problema 1 ---------------------------------------------------\n");
+        //printf("Problema 1 ---------------------------------------------------\n");
     }
     else
     {
         struct ieee80211_radiotap_header *radiotap = (struct ieee80211_radiotap_header *)packet;
         if (header->caplen < radiotap->it_len + sizeof(struct mgmt_header_t))
         {
-            printf("Problema 2 ---------------------------------------------------\n");
+            //printf("Problema 2 ---------------------------------------------------\n");
         }
         else
         {
             struct mgmt_header_t *mgmt_frame = (struct mgmt_header_t *)(packet + radiotap->it_len);
             printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x - %02x:%02x:%02x:%02x:%02x:%02x - %02x:%02x:%02x:%02x:%02x:%02x\n", mgmt_frame->da[0],mgmt_frame->da[1],mgmt_frame->da[2],mgmt_frame->da[3],mgmt_frame->da[4],mgmt_frame->da[5],mgmt_frame->sa[0],mgmt_frame->sa[1],mgmt_frame->sa[2],mgmt_frame->sa[3],mgmt_frame->sa[4],mgmt_frame->sa[5],mgmt_frame->bssid[0],mgmt_frame->bssid[1],mgmt_frame->bssid[2],mgmt_frame->bssid[3],mgmt_frame->bssid[4],mgmt_frame->bssid[5]);
+            printf(FC: %04x %02x %02x\n,mgmt_frame->fc,mgmt_frame->fc[0],mgmt_frame->fc[1]);
         }
     }
     
