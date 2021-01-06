f8602f257f575193b4aa162a94ecf8b624d8cd0d
Liviu Strati <liviu.strati@hdt.ro>
2021-01-06 11:33:44 +0200

    extract ssid from probe

--- scanner.c
+++ scanner.c
@@ -188,7 +188,7 @@ void get_frame_parameters(const u_char *packet, const struct pcap_pkthdr *header
                 case Beacon:
                     strcpy(rd.frameSubtype, "Beacon");
                     int location = *packet + radiotap->it_len + sizeof(struct mgmt_header_t);
-                    printf("Element ID: %u Element Length: %u\n",packet[location],packet[location+1]);
+                    printf("Location: %d Element ID: %u Element Length: %u\n",location,packet[location],packet[location+1]);
                     break;
                 case AssociationRequest:
                     strcpy(rd.frameSubtype, "AssociationRequest");
